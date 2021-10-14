a83c0403f5b7ada65b82d300881f8bdb22a6df57
Daniel Cargan <daniel@3wm.dkl>
2021-10-14 14:34:18 +0200

    first commit

---
+++ assets/nav.js
@@ -0,0 +1,47 @@
+var body = document.querySelector("body");
+var hamburger = document.querySelector(".hamburger");
+var menuContainer = document.querySelector(".menu-header");
+var menu = document.querySelector(".navmenu-header");
+var links = document.querySelectorAll(".navmenu-header li");
+var items = document.querySelectorAll(".nav-item");
+
+
+hamburger.addEventListener('click', ()=>{
+   //Menu behavior
+    menuContainer.classList.toggle("mobile-active");
+    menu.classList.toggle("open");
+    body.classList.toggle("scroll-overflow");
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
+
+
+/* Activate Submenu */
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+/* Event Listeners */
+
+for (let item of links) {
+  if (item.querySelector(".sub-menu")) {
+    item.addEventListener("click", toggleItem, false);
+  }
+  item.addEventListener("keypress", toggleItem, false);
+}
+document.addEventListener("click", closeSubmenu, false);
